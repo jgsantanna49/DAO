4c886f5d04fb4a255c919d4ac14818f17e2cee9d
Jorge <jgsantanna.brasil@gmail.com>
2018-07-23 20:49:16 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php 
 require_once("config.php");
 
+//carrega um usuário
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//echo $usuario;
+
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de usuários buscando pelo login com LIKE
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
 $usuario = new Usuario();
-$usuario->loadById(8);
+$usuario->login("Alisson","12345");
 echo $usuario;
 
  ?>
